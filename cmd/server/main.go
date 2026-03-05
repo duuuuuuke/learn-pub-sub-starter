4ed685949784f0be9b7afe5888835204a18210af
duke <dukebai8383@gmail.com>
2026-03-05 16:31:47 +1000

    Connect to RabbitMQ and publish message to exchange

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	const rabbitConnString = "amqp://guest:guest@localhost:5672/"
+
 	fmt.Println("Starting Peril server...")
+
+	conn, err := amqp.Dial(rabbitConnString)
+	if err != nil {
+		log.Fatalf("failed to connect to RabbitMQ server: %v", err)
+	}
+	defer conn.Close()
+	fmt.Println("Peril game server connected to RabbitMQ!")
+
+	channel, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("failed to open a channel: %v", err)
+	}
+	defer channel.Close()
+
+	err = pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+	if err != nil {
+		log.Fatalf("failed to publish message: %v", err)
+	}
+
+	// wait for ctrl+c to exit
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("RabbitMQ connection closed.")
 }
